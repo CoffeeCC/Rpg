6d002d2fcd1b26f5cca12249a67e23849bc475d1
CoffeeCC <>
2021-06-02 23:59:55 +0000

    choices

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const input = require("readline-sync");
 console.log("yawp what if i want it to do a bunch of lines of code will it go to the next line if i keep typing txt here?\n");
 var CharacterName = input.question("Please enter your name... \n\n");
 
-console.log(CharacterName);
+console.log("Characters name is", CharacterName,"\n\n");
+var CharacterNameYN = input.question("...Are you sure? (Type Yes or No \n\n)");
+if (CharacterNameYN == Yes) {
+  var CharacterRace = input.question("Please Choose a Race\n");
+}
