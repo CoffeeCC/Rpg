b0fab071185a9f477451c5d9ef294a779d62f5da
CoffeeCC <>
2021-06-02 23:40:21 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const input = require("readline-sync");
+console.log("yawp what if i want it to do a bunch of lines of code will it go to the next line if i keep typing txt here?\n");
+var CharacterName = input.question("Please enter your name... \n\n");
+
+console.log(CharacterName);
